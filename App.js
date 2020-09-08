799254c766fb21bb807e15b19b8b80ef68ab1a01
JCamiloo <jcamilo.osorio15@gmail.com>
2020-09-08 11:52:39 -0500

    cart reducer

--- App.js
+++ App.js
@@ -2,15 +2,19 @@ import React, { useState } from "react";
 import { createStore, combineReducers } from "redux";
 import { Provider } from "react-redux";
 import { AppLoading } from "expo";
+// import { composeWithDevTools } from "redux-devtools-extension";
 import * as Font from "expo-font";
 
 import productsReducer from "./store/reducers/products";
+import cartReducer from "./store/reducers/cart";
 import ShopNavigator from "./navigation/ShopNavigator";
 
 const rootReducer = combineReducers({
   products: productsReducer,
+  cart: cartReducer,
 });
 
+// const store = createStore(rootReducer, composeWithDevTools());
 const store = createStore(rootReducer);
 
 const fetchFonts = () => {
