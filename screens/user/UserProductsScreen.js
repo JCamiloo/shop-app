6eea19bfc1f6553c7cbc2a7174475283037e3614
JCamiloo <jcamilo.osorio15@gmail.com>
2020-09-12 00:53:05 -0500

    deleting items

--- screens/user/UserProductsScreen.js
+++ screens/user/UserProductsScreen.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { FlatList, Button, Platform } from "react-native";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 import CustomHeaderButton from "../../components/UI/HeaderButton";
 import ProductItem from "../../components/shop/ProductItem";
 import Colors from "../../constants/Colors";
+import * as productsActions from "../../store/actions/products";
 
 const UserProductsScreen = (props) => {
   const userProducts = useSelector((state) => state.products.userProducts);
+  const dispatch = useDispatch();
   return (
     <FlatList
       data={userProducts}
@@ -20,7 +22,13 @@ const UserProductsScreen = (props) => {
           onSelect={() => {}}
         >
           <Button color={Colors.primary} title="Edit" onPress={() => {}} />
-          <Button color={Colors.primary} title="Delete" onPress={() => {}} />
+          <Button
+            color={Colors.primary}
+            title="Delete"
+            onPress={() => {
+              dispatch(productsActions.deleteProduct(itemData.item.id));
+            }}
+          />
         </ProductItem>
       )}
     />
