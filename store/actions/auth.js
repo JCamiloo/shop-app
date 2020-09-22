f19487de30182acf505b20084cad02a4ca810797
JCamiloo <jcamilo.osorio15@gmail.com>
2020-09-22 11:00:57 -0500

    implementing auto login

--- store/actions/auth.js
+++ store/actions/auth.js
@@ -1,5 +1,9 @@
-export const SIGNUP = "SIGNUP";
-export const LOGIN = "LOGIN";
+import AsyncStorage from "@react-native-community/async-storage";
+export const AUTHENTICATE = "AUTHENTICATE";
+
+export const authenticate = (userId, token) => {
+  return { type: AUTHENTICATE, userId, token };
+};
 
 export const signup = (email, password) => {
   return async (dispatch) => {
@@ -25,7 +29,11 @@ export const signup = (email, password) => {
     }
 
     const resData = await response.json();
-    dispatch({ type: SIGNUP, token: resData.idToken, userId: resData.localId });
+    dispatch(authenticate(resData.localId, resData.idToken));
+    const expirationDate = new Date(
+      new Date().getTime() + parseInt(resData.expiresIn) * 1000
+    );
+    saveDataToStorage(resData.idToken, resData.localId, expirationDate);
   };
 };
 
@@ -55,6 +63,17 @@ export const login = (email, password) => {
     }
 
     const resData = await response.json();
-    dispatch({ type: LOGIN, token: resData.idToken, userId: resData.localId });
+    dispatch(authenticate(resData.localId, resData.idToken));
+    const expirationDate = new Date(
+      new Date().getTime() + parseInt(resData.expiresIn) * 1000
+    );
+    saveDataToStorage(resData.idToken, resData.localId, expirationDate);
   };
+};
+
+const saveDataToStorage = (token, userId, expirationDate) => {
+  AsyncStorage.setItem(
+    "userData",
+    JSON.stringify({ token, userId, expiryDate: expirationDate.toISOString() })
+  );
 };
--- store/reducers/auth.js
+++ store/reducers/auth.js
@@ -1,4 +1,4 @@
-import { LOGIN, SIGNUP } from "../actions/auth";
+import { AUTHENTICATE } from "../actions/auth";
 
 const initialState = {
   token: null,
@@ -7,15 +7,10 @@ const initialState = {
 
 export default (state = initialState, action) => {
   switch (action.type) {
-    case LOGIN:
+    case AUTHENTICATE:
       return {
         token: action.token,
         userId: action.userId,
-      };
-    case SIGNUP:
-      return {
-        token: action.token,
-        userId: actions.userId,
       };
     default:
       return state;
