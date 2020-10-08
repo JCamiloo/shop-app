9a6622e764705bd269d478cd486ff4e1fe2919b6
JCamiloo <jcamilo.osorio15@gmail.com>
2020-10-08 12:09:40 -0500

    setup for push notifications

--- store/actions/products.js
+++ store/actions/products.js
@@ -1,4 +1,6 @@
 import Product from '../../models/product';
+import * as Notifications from 'expo-notifications';
+import * as Permissions from 'expo-permissions';
 
 export const DELETE_PRODUCT = 'DELETE_PRODUCT';
 export const CREATE_PRODUCT = 'CREATE_PRODUCT';
@@ -63,6 +65,16 @@ export const deleteProduct = (productId) => {
 
 export const createProduct = (title, description, imageUrl, price) => {
   return async (dispatch, getState) => {
+    let pushToken;
+    let statusObj = await Permissions.getAsync(Permissions.NOTIFICATIONS);
+    if (statusObj.status !== 'granted') {
+      statusObj = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+    }
+    if (statusObj.status !== 'granted') {
+      pushToken = null;
+    } else {
+      pushToken = (await Notifications.getExpoPushTokenAsync()).data;
+    }
     const token = getState().auth.token;
     const userId = getState().auth.userId;
     const response = await fetch(
@@ -77,7 +89,8 @@ export const createProduct = (title, description, imageUrl, price) => {
           description,
           imageUrl,
           price,
-          ownerId: userId
+          ownerId: userId,
+          ownerPushToken: pushToken
         })
       }
     );
